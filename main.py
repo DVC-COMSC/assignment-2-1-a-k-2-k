eed5e0b0f2b5dae2add1edcfb6e965a2191a149d
a-k-2-k <akshaj.molukutla@gmail.com>
2023-06-19 10:19:38 -0700

    Send the file

--- main.py
+++ main.py
@@ -11,6 +11,18 @@ def main():
     # Do not delete the return statement
     ########################################
     """
+    maleStudents = int(input("How many male students are in the class? "))
+    femaleStudents = int(input("How many female students are in the class? "))
+    totalStudents = maleStudents + femaleStudents
+
+    m_perc = maleStudents * 100/totalStudents
+    f_perc = femaleStudents * 100/totalStudents
+
+    print("The total number of students: " + str(totalStudents))
+    print("The number of male and female students: " + str(maleStudents)+ " " + str(femaleStudents))
+    print("The percentage of males and females: " + str(m_perc)+ " " + str(f_perc)) 
+
+
     return m_perc, f_perc
 
 
